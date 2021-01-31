c1020c6bc95ea5223820825d0401f19e6ec02c3d
Luis Panadero Guardeño <luis.panadero@gmail.com>
2021-01-31 10:27:06 +0100

    Improvements to build_nml.py script
    * Now defines a Loader to allow to use #include and include other
    Velocity templates
    * Added --debug option to display the read data
    * Less verbose output

--- tools/build_nml.py
+++ tools/build_nml.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 OUTPUT_PATH = "generated/"
 
-
 def parseArguments():
   "Parse command line arguments and get the type of engine that will be parsed"
   import argparse
 
   parser = argparse.ArgumentParser(prog='build_nml', description='Build NML files from Velocity templates.')
   parser.add_argument("engine", choices=['steam', 'diesel', 'electric'])
+  parser.add_argument("-d","--debug", action='store_const', const=True, help="Enables the debug mode.")
   args = parser.parse_args()
-  return args.engine
+  return [args.debug, args.engine]
 
 def stringToNumber(strValue):
   "Tries to convert a string to a number (integer or float)"
@@ -50,28 +50,45 @@ def writeOutputFile(filename, data):
   "Writes a file"
   import codecs # Easy I/O
 
-  outputFile = codecs.open(filename, 'w', 'utf-8')
-  outputFile.write(data)
-  outputFile.close()
+  try:
+    outputFile = codecs.open(filename, 'w', 'utf-8')
+    outputFile.write(data)
+  finally:
+    outputFile.close()
+
+
+class Loader:
+  """Template loader for Airspeed"""
+  def load_text(self, name):
+    try:
+      inputFile = codecs.open(name, 'r', 'utf-8')
+      text = inputFile.read()
+    finally:
+      inputFile.close()
+    return text
 
 def parseTemplate(template, data, engine, outputFilename):
   "Parses a Airspeed template and generates an output file"
-  templateOutput = template.merge(data)
+
+  templateOutput = template.merge(data, loader=Loader())
 
   print(f"Generating file : {outputFilename}")
   writeOutputFile(OUTPUT_PATH + engine + "/" + outputFilename, templateOutput)
 
 
 def main():
-  engine = parseArguments()
+  debug, engine = parseArguments()
+  if debug:
+    print("Debug mode activated")
+
   print("Generating nml files for " + engine)
 
   import codecs # Easy I/O
   import airspeed # Tamplete engine compatible with Java Velocity
 
-
   data = readCSVFile(f'src/{engine}/specs.csv')
-  #print(data)
+  if debug:
+    print(data)
 
   # Generate all the vehicle files from CSV data
   itemTemplate = airspeed.Template(codecs.open(f'src/{engine}/template_item.vm', 'r', 'utf-8').read())
@@ -79,11 +96,9 @@ def main():
 
   ids=[]
   for engineData in data:
-    print(f"Templating item file for: {engineData['id']}")
     outputItemFilename = f"{engineData['id']}_item.pnml"
     parseTemplate(itemTemplate, engineData, engine, outputItemFilename);
 
-    print(f"Templating graphics file for: {engineData['id']}")
     outputGraphicsFilename = f"{engineData['id']}_graphics.pnml"
     parseTemplate(graphicsTemplate, engineData, engine, outputGraphicsFilename);
 
@@ -91,7 +106,6 @@ def main():
 
   # Generate the include all pnml file
   includeAllTemplate = airspeed.Template(codecs.open(f'src/{engine}/template_include.vm', 'r', 'utf-8').read())
-  print(f"Templating file {engine}/template_include.vm")
   parseTemplate(includeAllTemplate, {'ids': ids}, engine, 'include_all.pnml');
 
 
