2126e64ec2545889f2b99d6fe04e7c1df033287b
Montana <montymontana.m>
2021-04-16 16:22:51 +0200

    Fix: compile script.

--- tools/compile_spain_set.sh
+++ tools/compile_spain_set.sh
@@ -1,14 +1,14 @@
-# Small script for comiling the latest master version of Spain Set.
+# Small script for compiling the latest master version of Spain Set.
 
 # Features:
-# 1 - Removes previous files
-# 2 - Downloads an unzips latest commit
-# 3 - Installs the dependencies for compiling
-# 4 - Builds the set
+# 1 - Removes previous files.
+# 2 - Downloads an unzips latest commit.
+# 3 - Installs the dependencies for compiling.
+# 4 - Builds the set from source.
 
-# Usage: schedule it at the crontab for periodic execution
+# Usage: schedule it at the crontab for periodic execution.
 
-# Tested on Ubuntu 20.04 - Apr-2020
+# Tested on Ubuntu 20.04 - Apr-2020.
 
 #################################################################
 
@@ -17,8 +17,5 @@ rm -r spainset-engines-master
 wget https://github.com/spainishset/spainset-engines/archive/refs/heads/master.zip
 unzip master.zip
 cd spainset-engines-master
-cd tools
-pip3 install .
-cd ..
-./build.sh |& tee build_log.txt
+./build.sh | tee build_log.txt
 echo EXECUTION FINISHED
